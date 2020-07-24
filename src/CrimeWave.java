e5cec9a20ce696da2e02222a86544197ffb7c7a3
Deepak Koli <Deepakkoli93@gmail.com>
2020-07-24 21:17:32 +0530

    flow

--- src/CrimeWave.java
+++ src/CrimeWave.java
@@ -5,7 +5,18 @@ import java.util.*;
 
 public class CrimeWave {
 
+    static class Pair {
+        int node;
+        double weight;
+        Pair(int n, double w) {
+            this.node = n;
+            this.weight = w;
+        }
+    }
+
     static class MaxFlow {
+
+
 
         private List<List<Integer>> adjList;
         private int[][] capacity;
@@ -15,7 +26,7 @@ public class CrimeWave {
         private int n;
         private double[] dist;
 
-        private final static double EPSILON = 0.000001;
+        private final static double EPSILON = 1e-9;
         public static boolean isEqual(double a, double b){
             return Math.abs(a - b) < EPSILON;
         }
@@ -66,10 +77,11 @@ public class CrimeWave {
                 flow[parent][curr] += minResCap;
                 flow[curr][parent] -= minResCap;
 
-//                System.out.println("e1 - " + parent + " e2 - " + curr + " flow = " + minResCap + " cost = " + cost[parent][curr]);
+//                System.out.println("e1 - " + parent + " e2 - " + curr + " flow = " + minResCap + " cost = " + getCost(parent, curr, cost));
                 curr = parent;
             }
 //            System.out.println("==============================================");
+//            System.out.println(minResCap);
             return new double[]{minResCap, totalCost};
         }
 
@@ -86,25 +98,28 @@ public class CrimeWave {
             Arrays.fill(dist, Integer.MAX_VALUE);
             Arrays.fill(bfsParent, -1);
             visited = new boolean[n];
-            dist[source] = 0;
+            dist[source] = 0.0;
             visited[source] = true;
             // weight, node
-            Queue<double[]> q = new PriorityQueue<>(Comparator.comparingDouble(o -> o[0]));
-            q.add(new double[]{0, source});
+            Queue<Pair> q = new PriorityQueue<>(Comparator.comparingDouble(o -> o.weight));
+            q.add(new Pair(source, 0.0));
             while(!q.isEmpty()) {
-                double[] front = q.poll();
-                visited[(int)front[1]] = true;
-                if ((int)front[1] == sink) break;
-                if(isEqual(front[0], dist[(int)front[1]])) {
-                    for(int nei: adjList.get((int)front[1])) {
-                        int resCap = resCapacity((int)front[1], nei);
-                        double x = dist[(int)front[1]];
-                        double costOfEdge = getCost((int)front[1], nei, cost);
+                Pair front = q.poll();
+//                double[] front = q.poll();
+                visited[front.node] = true;
+
+                if(isEqual(front.weight, dist[front.node])) {
+                    if (front.node == sink) break;
+                    for(int nei: adjList.get(front.node)) {
+                        int resCap = resCapacity(front.node, nei);
+
+                        double x = dist[front.node];
+                        double costOfEdge = getCost(front.node, nei, cost);
                         double y = dist[nei];
-                        if(resCap > 0 &&  x + costOfEdge + EPSILON < y) {
-                            dist[nei] = dist[(int)front[1]] + getCost((int)front[1], nei, cost);
-                            q.add(new double[]{dist[nei], nei});
-                            bfsParent[nei] = (int)front[1];
+                        if(resCap > 0 &&  x + costOfEdge  < y) {
+                            dist[nei] = dist[front.node] + getCost(front.node, nei, cost);
+                            q.add(new Pair(nei, dist[nei]));
+                            bfsParent[nei] = front.node;
                         }
                     }
                 }
@@ -122,9 +137,10 @@ public class CrimeWave {
                 totalCost += res[1];//res[0] * dist[t];
             }
 //            System.out.println("flow = " + maxFlow);
+            totalCost = totalCost/num;
             totalCost = Math.floor(totalCost * 100 + 0.5 + EPSILON)/100.0;
-            double avg = totalCost/num;
-            System.out.println(String.format("%.2f", avg));
+//            double avg = totalCost/num;
+            System.out.println(String.format("%.2f", totalCost));
 
         }
     }
@@ -137,7 +153,7 @@ public class CrimeWave {
             int m = Integer.parseInt(arr[1]);
             if(n==0 && m==0) break;
             double[][] cost = new double[n+m+2][n+m+2];
-            for(double[] row: cost) Arrays.fill(row, Integer.MAX_VALUE);
+            for(double[] row: cost) Arrays.fill(row, 1<<30);
             MaxFlow mf = new MaxFlow(m+n+2);
             // start = 0
             // end = n+m+1
@@ -145,13 +161,13 @@ public class CrimeWave {
                 cost[0][i] = 0;
                 cost[i][0] = 0;
                 mf.makeEdge(0, i, 1);
-                mf.makeEdge(i, 0, 1);
+//                mf.makeEdge(i, 0, 1);
             }
             for(int i=m+1; i<=m+n; i++) {
                 cost[i][m+n+1] = 0;
                 cost[m+n+1][i] = 0;
                 mf.makeEdge(i, m+n+1, 1);
-                mf.makeEdge(m+n+1, i, 1);
+//                mf.makeEdge(m+n+1, i, 1);
             }
             for(int i=0; i<n; i++) {
                 arr = reader.readLine().split("\\s+");
@@ -162,7 +178,7 @@ public class CrimeWave {
                     cost[cruiserInd][bankInd] = c;
                     cost[bankInd][cruiserInd] = c;
                     mf.makeEdge(cruiserInd, bankInd, 1);
-                    mf.makeEdge(bankInd, cruiserInd, 1);
+//                    mf.makeEdge(bankInd, cruiserInd, 1);
                 }
             }
             mf.run(0, m+n+1, cost, n);
